c0e049b5bd5c4ff03a0c46129edaff1fc3c81528
kaiyes <kaiyes.ansary@gmail.com>
2020-05-21 16:24:37 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native-web'
 
-function App() {
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+function Home({ navigation: { navigate } }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <View style={styles.rootContainer}>
+      <TouchableOpacity style={styles.button} onPress={() => navigate('About')}>
+        <Text style={styles.buttonText}>Home page</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-export default App;
+function About({ navigation: { navigate } }) {
+  return (
+    <View style={styles.rootContainer}>
+      <TouchableOpacity style={styles.button} onPress={() => navigate('Home')}>
+        <Text style={styles.buttonText}>About Page</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'seagreen'
+  },
+  button: {
+    backgroundColor: 'gold',
+    borderRadius: 5,
+    width: 200,
+    marginVertical: 10
+  },
+  buttonText: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    fontSize: 15,
+    fontWeight: '400'
+  }
+})
